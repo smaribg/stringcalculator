58980f3e8b643403277114260bdb569dfbaca96f
Smari <smarig15@ru.is>
2016-10-12 10:07:42 +0000

    two numbers refactored

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -3,11 +3,11 @@ package is.ru.stringcalculator;
 public class Calculator{
 
 	public static int add(String text){
-		if(emptyString(text))
+		if(emptyString(text)){
 			return 0;
+		}
 		else if(text.contains(",")){
-			String [] numbers = text.split(",");
-			return toInt(numbers[0]) + toInt(numbers[1]);
+			return sum(splitText(text));
 		}
 		else{
 			return toInt(text);
@@ -17,7 +17,17 @@ public class Calculator{
 	private static boolean emptyString(String text){
 		return (text == " ");
 	}
+
 	private static int toInt(String text){
 		return Integer.parseInt(text);
 	}
+
+	private static int sum(String[] numbers){
+		return toInt(numbers[0]) + toInt(numbers[1]);
+	}
+
+	private static String[] splitText(String text){
+		return text.split(",");
+	}
+
 }
